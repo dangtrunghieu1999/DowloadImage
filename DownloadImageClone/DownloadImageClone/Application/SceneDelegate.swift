f210c8b6637cd2ca9979a6bad64f1de1e12dd674
dangtrunghieu1999 <hieudt230899@gmail.com>
2022-02-22 14:01:05 +0700

    add pod RxSwift, RxCoCoa, SD_webImage

---
+++ DownloadImageClone/DownloadImageClone/Application/SceneDelegate.swift
@@ -0,0 +1,51 @@
+//
+//  SceneDelegate.swift
+//  DownloadImageClone
+//
+//  Created by Bee_MacPro on 15/02/2022.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+    
+    @available(iOS 13.0, *)
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        self.setupWindow(windowScene: windowScene)
+    }
+    
+    @available(iOS 13.0, *)
+    func setupWindow(windowScene: UIWindowScene) {
+        
+    }
+    
+    @available(iOS 13.0, *)
+    func sceneDidDisconnect(_ scene: UIScene) {
+
+    }
+    
+    @available(iOS 13.0, *)
+    func sceneDidBecomeActive(_ scene: UIScene) {
+
+    }
+    
+    @available(iOS 13.0, *)
+    func sceneWillResignActive(_ scene: UIScene) {
+
+    }
+    
+    @available(iOS 13.0, *)
+    func sceneWillEnterForeground(_ scene: UIScene) {
+    }
+    
+    @available(iOS 13.0, *)
+    func sceneDidEnterBackground(_ scene: UIScene) {
+
+    }
+
+}
+
---
+++ DownloadImageClone/DownloadImageClone/Presentation/ListPhoto/Views/ListPhotoViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ListPhotoViewController.swift
+//  DownloadImageClone
+//
+//  Created by Bee_MacPro on 22/02/2022.
+//
+
+import UIKit
+
+class ListPhotoViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
